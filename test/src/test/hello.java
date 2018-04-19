e4dfcf6bf83875d70e644fbc348a3e66f449a6f8
long <army321@126.com>
2018-04-20 00:45:16 +0800

    Initial commit

---
+++ test/src/test/hello.java
@@ -0,0 +1,13 @@
+package test;
+
+public class hello {
+	public static void main(String[] args) {
+        System.out.println("Hello World");
+        
+        int x = 53;
+        System.out.println(1.0 + x/2);
+        
+    }
+
+}
+
